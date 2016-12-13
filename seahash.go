3255a1141614525f01a8e47b113601916e6e9381
Dimitri Sokolyuk <demon@dim13.org>
2016-12-13 01:45:09 +0100

    Initial import

---
+++ seahash.go
@@ -0,0 +1,63 @@
+// Package seahash implements SeaHash algorithm
+package seahash
+
+import "hash"
+
+// Size of SeaHash sum in bytes
+const Size = 8
+
+type digest struct {
+	sum, a, b, c, d uint64
+}
+
+// New returns a new hash.Hash64 computing SeaHash
+func New() hash.Hash64 {
+	return &digest{
+		0,
+		0x16f11fe89b0d677c,
+		0xb480a793d8e6c86c,
+		0x6fe2e5aaf078ebc9,
+		0x14f994a4c5259381,
+	}
+}
+
+func diffuse(x uint64) uint64 {
+	x *= 0x6eed0e9da4d94a4f
+	a := x >> 32
+	b := x >> 60
+	x ^= a >> b
+	x *= 0x6eed0e9da4d94a4f
+	return x
+}
+
+func (d *digest) Write(p []byte) (n int, err error) {
+	n = len(p)
+	for len(p) > 0 {
+		var q []byte
+		if len(p) > Size {
+			p, q = p[:Size], p[Size:]
+		}
+		var x uint64
+		for i := len(p) - 1; i >= 0; i-- {
+			x <<= 8
+			x |= uint64(p[i])
+		}
+		d.a, d.b, d.c, d.d = d.b, d.c, d.d, diffuse(d.a^x)
+		p = q
+	}
+	d.sum = diffuse(d.a ^ d.b ^ d.c ^ d.d ^ uint64(n))
+	return n, nil
+}
+
+func (d *digest) Sum(b []byte) []byte {
+	s := d.Sum64()
+	for i := Size - 1; i >= 0; i-- {
+		b = append(b, byte(s>>uint(8*i)))
+	}
+	return b
+}
+
+func (d *digest) Reset()         { d.sum = 0 }
+func (d *digest) Size() int      { return Size }
+func (d *digest) BlockSize() int { return 1 }
+func (d *digest) Sum64() uint64  { return d.sum }
---
+++ seahash_test.go
@@ -0,0 +1,82 @@
+package seahash
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestHash(t *testing.T) {
+	testCases := []struct {
+		s string
+		n uint64
+	}{
+		{"to be or not to be", 1988685042348123509},
+		{"love is a wonderful terrible thing", 4784284276849692846},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.s, func(t *testing.T) {
+			d := New()
+			d.Write([]byte(tc.s))
+			if sum := d.Sum64(); sum != tc.n {
+				t.Errorf("got %v, want %v", sum, tc.n)
+			}
+		})
+	}
+}
+
+func TestNotEqual(t *testing.T) {
+	testCases := []struct {
+		a, b string
+	}{
+		{"to be or not to be ", "to be or not to be"},
+		{"jkjke", "jkjk"},
+		{"ijkjke", "ijkjk"},
+		{"iijkjke", "iijkjk"},
+		{"iiijkjke", "iiijkjk"},
+		{"iiiijkjke", "iiiijkjk"},
+		{"iiiiijkjke", "iiiiijkjk"},
+		{"iiiiiijkjke", "iiiiiijkjk"},
+		{"iiiiiiijkjke", "iiiiiiijkjk"},
+		{"iiiiiiiijkjke", "iiiiiiiijkjk"},
+		{"ab", "bb"},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.b, func(t *testing.T) {
+			a, b := New(), New()
+			a.Write([]byte(tc.a))
+			b.Write([]byte(tc.b))
+			if a.Sum64() == b.Sum64() {
+				t.Fail()
+			}
+		})
+	}
+}
+
+func TestZeroSenitive(t *testing.T) {
+	testCases := []struct {
+		a, b []byte
+	}{
+		{[]byte{1, 2, 3, 4}, []byte{1, 0, 2, 3, 4}},
+		{[]byte{1, 2, 3, 4}, []byte{1, 0, 0, 2, 3, 4}},
+		{[]byte{1, 2, 3, 4}, []byte{1, 2, 3, 4, 0}},
+		{[]byte{1, 2, 3, 4}, []byte{0, 1, 2, 3, 4}},
+		{[]byte{0, 0, 0}, []byte{0, 0, 0, 0, 0}},
+	}
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("% x", tc.b), func(t *testing.T) {
+			a, b := New(), New()
+			a.Write(tc.a)
+			b.Write(tc.b)
+			if a.Sum64() == b.Sum64() {
+				t.Fail()
+			}
+		})
+	}
+}
+
+func BenchmarkHash(b *testing.B) {
+	d := New()
+	for i := 0; i < b.N; i++ {
+		d.Write([]byte("to be or not to be"))
+	}
+}
